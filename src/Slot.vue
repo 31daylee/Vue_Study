b3ec2aa8d9f8d8a44867809e321126831050ea3b
31daylee <31daylee@gmail.com>
2023-11-07 09:57:42 +0900

    14.Slot / Vue_Tutorial DONE

---
+++ src/Slot.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import ChildComp from "./ChildComp2.vue";
+
+const msg = ref("Show me the message!");
+</script>
+
+<template>
+  <ChildComp>msg : {{ msg }}</ChildComp>
+</template>
